7fba18fb05c96cdb0b0a84ea996f68e90b7df5e6
netordot <sdenisdv1@gmail.com>
2024-11-29 11:02:21 +0300

    implementation of book and author services

---
+++ TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+public class AuthorService : IAuthorService
+{
+    private readonly DateTime DATE_2015 = new DateTime(2015, 12, 31);
+    private readonly ApplicationDbContext _context;
+
+    public AuthorService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    /// <summary>
+    /// Method for getting the author with the longest book title
+    /// </summary>
+    /// <returns></returns>
+    public async Task<Author> GetAuthor()
+    {
+        var authorWithLongestTitle = await _context.Authors
+            .Select(a => new
+            {      
+                Author = a,
+                LongestTitleLength = a.Books.Max(b => b.Title.Length)
+            })
+            .OrderByDescending(a => a.LongestTitleLength)
+            .ThenBy(a => a.Author.Id)
+            .FirstOrDefaultAsync();
+
+        return authorWithLongestTitle?.Author;
+    }
+
+    /// <summary>
+    /// Method for getiing the authors who wrote even number of books after 2015
+    /// /// </summary>
+    /// <returns></returns>
+    public async Task<List<Author>> GetAuthors()
+    {
+        var authors = await _context.Authors
+            .Where(a => a.Books.Count(b => b.PublishDate > DATE_2015) % 2 == 0)
+            .ToListAsync();
+
+        return authors;
+    }
+
+}
---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class BookService : IBookService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DateTime CAROLUS_REX_RELEASE_DATE = new DateTime(2012, 5, 2);
+
+        public BookService(ApplicationDbContext context)
+        {
+            _context = context; 
+        }
+        /// <summary>
+        /// Method for getting the book with the biggest cost of circulation
+        /// </summary>
+        /// <returns>Book</returns>
+        public async Task<Book> GetBook()
+        {
+            var book = _context.Books
+                .Select(book => new
+                {
+                    Book = book,
+                    TotalPrice = book.Price * book.QuantityPublished
+                })
+                .OrderByDescending(book => book.TotalPrice)
+                .FirstOrDefault();
+
+            return book.Book;
+        }
+        /// <summary>
+        /// Method for getting the books that have "Red" in their name and were published after "Carolus rex" release
+        /// </summary>
+        /// <returns>List<Book></returns>
+        public async Task<List<Book>> GetBooks()
+        {
+            var books = await _context.Books
+                .Where(b => b.Title.Contains("Red") && b.PublishDate > CAROLUS_REX_RELEASE_DATE)
+                .ToListAsync();
+
+            return books;       
+        }
+    }
+}
